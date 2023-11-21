265e49dbbd0cf07625fc8f0551ab8dbb21b67174
sammels <sasagete@gmail.com>
2023-11-21 14:57:24 +0300

    download books from tululu.org.

---
+++ ParseE-lib.py
@@ -0,0 +1,26 @@
+import requests
+from pathlib import Path
+
+def download_books_txt():
+    """Скачивает книги в txt формате."""
+    books = range(1, 10)
+
+    for book in books:
+        url = f"https://tululu.org/txt.php?id={book}"
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        with open(f'books/id{book}.txt', 'wb') as file:
+            file.write(response.content)
+
+def create_download_directory():
+    Path("/home/sammels/Документы/Work/Devman/DevmanCourse_week28/ParseOnlineLib/books/").mkdir(parents=True, exist_ok=True)
+def main():
+    """Запускает исполнение скрипта."""
+    create_download_directory()
+    download_books_txt()
+
+
+if __name__ == "__main__":
+    main()
